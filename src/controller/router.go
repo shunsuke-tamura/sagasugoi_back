c97c0327adba745cb4c860537944e54310057330
shunsuke-tamura <wwwxx4869@gmail.com>
2022-10-10 14:23:52 +0900

    add: healthcheck追加

--- src/controller/router.go
+++ src/controller/router.go
@@ -1,11 +1,14 @@
 package controller
 
 import (
+	"sagasugoi/model"
+
 	"github.com/gin-gonic/gin"
 )
 
 func GetRouter() *gin.Engine {
 	r := gin.Default()
+	r.GET("/healthcheck", Healthcheck)
 	api := r.Group("/types")
 	{
 		api.GET("/", Index)
@@ -15,4 +18,11 @@ func GetRouter() *gin.Engine {
 		api.POST("/delete", DeleteType)
 	}
 	return r
+}
+
+func Healthcheck(c *gin.Context) {
+	status := model.Health{
+		Status: "OK",
+	}
+	c.JSON(200, status)
 }
